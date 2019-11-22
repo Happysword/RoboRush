1c23e386db8cf48e8eece56d12af38f20a45c47a
ahmedx3 <ahmed.moustafaa@outlook.com>
2019-11-22 23:04:03 +0200

    Updating Camera Position

--- src/scenes/MainGame.ts
+++ src/scenes/MainGame.ts
@@ -10,11 +10,15 @@ import { createElement, StatelessProps, StatelessComponent } from 'tsx-create-el
 
 export default class MainGame extends Scene {
     
+    time: number = 0;
+    roadTimer : number = 0;
     roadProgram: ShaderProgram;
     meshes: {[name: string]: Mesh} = {};
     textures: {[name: string]: WebGLTexture} = {};
     camera: Camera;
     controller: FlyCameraController;
+
+    roadMat : mat4;
 
     public load(): void {
         this.game.loader.load({
@@ -56,7 +60,7 @@ export default class MainGame extends Scene {
         this.camera = new Camera();
         this.camera.type = 'perspective';
         this.camera.position = vec3.fromValues(0,2,-2);
-        this.camera.direction = vec3.fromValues(0,-2,2);
+        this.camera.direction = vec3.fromValues(0,-0.5,1);
         this.camera.aspectRatio = this.gl.drawingBufferWidth/this.gl.drawingBufferHeight;
         
         /*******************************  Initializing camera controller (only for testing will be removed) *******************************/
@@ -72,6 +76,7 @@ export default class MainGame extends Scene {
     
     public draw(deltaTime: number): void {
         let VP = this.camera.ViewProjectionMatrix; // Universal View-Projection Matrix (Constant Through The Whole Game)
+        this.time += deltaTime / 1000;             // Time in seconds we use delta time to be consitant on all computers 
 
         this.controller.update(deltaTime); //Only For testing purposes (will be removed)
 
@@ -93,10 +98,13 @@ export default class MainGame extends Scene {
         this.meshes['road'] = null;
     }
 
-    public DrawRoad(VP : mat4) : void
+    public DrawRoad(VP : mat4) : mat4
     {
+        // Here we draw 2 planes as a start
+
+        // Start of first plane
         let roadMat = mat4.clone(VP);
-        mat4.rotateY(roadMat,roadMat, 90 * Math.PI / 180)
+        mat4.rotateY(roadMat,roadMat, -90 * Math.PI / 180)
         mat4.scale(roadMat, roadMat, [3, 1, 3]);
 
         this.roadProgram.setUniformMatrix4fv("MVP", false, roadMat);
@@ -107,6 +115,22 @@ export default class MainGame extends Scene {
         this.roadProgram.setUniform1i('texture_sampler', 0);
 
         this.meshes['road'].draw(this.gl.TRIANGLES);
+
+        // Start of second plane
+        mat4.translate(roadMat , roadMat , [2,0,0]);                     // increment x by 2 (distance between 2 planes)
+
+        this.roadProgram.setUniformMatrix4fv("MVP", false, roadMat);
+        this.roadProgram.setUniform4f("tint", [1, 1, 1, 1]);
+
+        this.gl.activeTexture(this.gl.TEXTURE0);
+        this.gl.bindTexture(this.gl.TEXTURE_2D, this.textures['road']);
+        this.roadProgram.setUniform1i('texture_sampler', 0);
+
+        this.meshes['road'].draw(this.gl.TRIANGLES);
+
+        //ToDo draw after n time
+
+        return roadMat;
     }
 
 }
